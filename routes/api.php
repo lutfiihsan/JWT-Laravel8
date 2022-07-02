1e4968d49521720841adeb7c5c5f5e99b635f154
dnatsu489 <lutficreativesys@gmail.com>
2022-07-02 18:10:52 +0700

    JWT part 1 - https://webdock.io/en/docs/how-guides/laravel-guides/anatomy-json-web-tokens-jwt

---
+++ routes/api.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::group([
+
+    'middleware' => 'api',
+    'prefix' => 'v1'
+
+], function ($router) {
+
+    Route::post('login', [App\Http\Controllers\AuthController::class, 'login']);
+    Route::post('logout', [App\Http\Controllers\AuthController::class, 'logout']);
+    Route::post('refresh', [App\Http\Controllers\AuthController::class, 'refresh']);
+    Route::post('me', [App\Http\Controllers\AuthController::class, 'me']);
+    Route::post('register', [App\Http\Controllers\AuthController::class, 'register']);
+});
+
+Route::any('{any}', function(){
+    return response()->json([
+        'status' => 'error',
+        'message' => 'Resource not found'], 404);
+})->where('any', '.*');
